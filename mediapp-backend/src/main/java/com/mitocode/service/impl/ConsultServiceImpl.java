519fd734c5380fafd8da058e930e6ab0eab1c2a3
Quertry <t4vo.98@gmail.com>
2024-04-28 21:00:24 -0500

    curso

---
+++ mediapp-backend/src/main/java/com/mitocode/service/impl/ConsultServiceImpl.java
@@ -0,0 +1,48 @@
+package com.mitocode.service.impl;
+
+import com.mitocode.model.Consult;
+import com.mitocode.model.Exam;
+import com.mitocode.repo.IConsultExamRepo;
+import com.mitocode.repo.IGenericRepo;
+import com.mitocode.repo.IConsultRepo;
+import com.mitocode.service.IConsultService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ConsultServiceImpl extends CRUDImpl<Consult, Integer> implements IConsultService {
+
+    //@Autowired
+    private final IConsultRepo consultRepo;// = new ConsultRepo();
+    private final IConsultExamRepo ceRepo;
+
+    @Override
+    protected IGenericRepo<Consult, Integer> getRepo() {
+        return consultRepo;
+    }
+
+    @Transactional
+    @Override
+    public Consult saveTransactional(Consult consult, List<Exam> exams) {
+        consultRepo.save(consult); //GUARDAR MAESTRO DETALLE
+        exams.forEach(ex -> ceRepo.saveExam(consult.getIdConsult(), ex.getIdExam())); //INSERTANDO EN TABLA CONSULT_EXAM
+
+        return consult;
+    }
+
+    @Override
+    public List<Consult> search(String dni, String fullname) {
+        return consultRepo.search(dni, fullname);
+    }
+
+    @Override
+    public List<Consult> searchByDates(LocalDateTime date1, LocalDateTime date2) {
+        final int OFFSET_DAYS = 1;
+        return consultRepo.searchByDates(date1, date2.plusDays(OFFSET_DAYS));
+    }
+}
